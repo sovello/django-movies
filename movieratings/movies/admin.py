42c65824154ef0bab62a012a06833153b34a9ba2
sovello <sovellohpmgani@gmail.com>
2015-06-09 22:45:40 -0400

    script uploads data and some functionalities added to admin module

--- movieratings/movies/admin.py
+++ movieratings/movies/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import *
+
 # Register your models here.
 
 class MovieAdmin(admin.ModelAdmin):
@@ -8,12 +9,26 @@ class MovieAdmin(admin.ModelAdmin):
     fields = ['title', 'genre', 'imdb_url', 'release_date', 'video_release_date']
 
     # This line changes the order fields are display on the view form
-    list_fields = ('title', 'release_date', 'imdb_url')
+    list_display = ('title', 'release_date', 'hyperlink')
 
+    #add/change the filters
+    list_filter = ['release_date', 'genre']
+    # to include search
+    search_fields = ['title']
+
+
+class RaterAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'gender', 'rated_movies')
+    #search_fields = ['occupation'] seems not to work with mapped fields
+
+    list_filter = ['occupation']
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'total_movies')
     
 admin.site.register(Occupation)
 admin.site.register(Gender)
-admin.site.register(Genre)
+admin.site.register(Genre, GenreAdmin)
 admin.site.register(Ratings)
 admin.site.register(Movie, MovieAdmin) # add this for the change to take effect
-admin.site.register(Rater)
+admin.site.register(Rater, RaterAdmin)
