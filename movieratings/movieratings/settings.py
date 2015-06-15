2f39281039fad6c83b285de9ccef31a96febe79c
Sovello Hildebrand <sovellohpmgani@gmail.com>
2015-06-15 12:25:08 -0400

    Update settings.py

--- movieratings/movieratings/settings.py
+++ movieratings/movieratings/settings.py
@@ -106,7 +106,7 @@ LOGIN_REDIRECT_URL = '/movies'
 LOGIN_URL = '/login/'
 
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'sovellohpmgani@gmail.com'
-EMAIL_HOST_PASSWORD = ',.;Sovello1984'
+EMAIL_HOST = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
